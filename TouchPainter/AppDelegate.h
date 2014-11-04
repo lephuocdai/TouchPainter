9fdad6a2c39f4c92f10a1f90681ae67db00f38ce
Phuoc Dai Le <dailp@mobilus.co.jp>
2014-11-04 11:20:21 +0900

    Initial Commit

---
+++ TouchPainter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchPainter
+//
+//  Created by Phuoc Dai Le on 11/4/14.
+//  Copyright (c) 2014 Phuoc Dai Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchPainter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchPainter
+//
+//  Created by Phuoc Dai Le on 11/4/14.
+//  Copyright (c) 2014 Phuoc Dai Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
